439df95823df7836fd8e1a6c1171091dd5eb0e4a
lakshaydadhich <75737644+lakshaydadhich@users.noreply.github.com>
2022-07-29 16:26:05 +0530

    my first commit reactwebsite

---
+++ src/component/calculator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const calculator = () => {
+    return(
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="display-6 fw-bolder">CALCULATOR</h1>
+                        <hr />
+                    </div>      
+                </div>   
+            </div>
+        </div>
+
+    );
+}
+export default calculator;
